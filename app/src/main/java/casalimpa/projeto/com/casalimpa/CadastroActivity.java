02d6d6c8ed887511f7b58eb76f527dea2b78b293
DanielM2305 <danielmarques2305@gmail.com>
2018-03-15 21:08:37 -0300

    Mascaras

--- app/src/main/java/casalimpa/projeto/com/casalimpa/CadastroActivity.java
+++ app/src/main/java/casalimpa/projeto/com/casalimpa/CadastroActivity.java
@@ -4,14 +4,83 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText telEditText;
+    private EditText cpfEditText;
+    private EditText dataEditText;
+    private EditText cepEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+        /*
+        Passos para criar a mascara]
+           Crie um id para o campo no arquvio XML
+           Crie uma variavel no java exemplo private EditText telEditText;
+           Dentro da classe onCreate
+           recupere o ID via findViewById
+           telEditText = (EditText) findViewById(R.id.telEditTextId);
+
+         Use os seguintes comandos de exemplo
+
+        SimpleMaskFormatter smfTel = new SimpleMaskFormatter("(NN)NNNNN NNNN");
+        MaskTextWatcher mtwTel = new MaskTextWatcher(telEditText, smfTel);
+        telEditText.addTextChangedListener(mtwTel);
+
+        */
+
+     //recuperando id
+        cpfEditText = (EditText) findViewById(R.id.cpfEditTextId);
+        telEditText = (EditText) findViewById(R.id.telEditTextId);
+        dataEditText = (EditText) findViewById(R.id.dataEditTextId);
+        cepEditText = (EditText) findViewById(R.id.cepEditTextId);
+
+
+        //Mascara de Cpf
+        SimpleMaskFormatter smfCpf = new SimpleMaskFormatter("NNN.NNN.NNN-NN");
+        MaskTextWatcher mtwCpf = new MaskTextWatcher(cpfEditText, smfCpf);
+        cpfEditText.addTextChangedListener(mtwCpf);
+
+        //Final da Mascara CPF
+
+        //Criando mascara para o campo de Telefone
+
+
+        SimpleMaskFormatter smfTel = new SimpleMaskFormatter("(NN)NNNNNNNNN");
+        MaskTextWatcher mtwTel = new MaskTextWatcher(telEditText, smfTel);
+        telEditText.addTextChangedListener(mtwTel);
+        //final da mascara Telefone
+
+        //Criando mascara para o campo de Data
+        SimpleMaskFormatter smfData = new SimpleMaskFormatter("NN/NN/NNNN");
+        MaskTextWatcher mtwData = new MaskTextWatcher(dataEditText, smfData);
+        dataEditText.addTextChangedListener(mtwData);
+        //final da mascara Data
+
+        //Criandao mascara para o campo de CEP
+        SimpleMaskFormatter smfCep = new SimpleMaskFormatter("NN.NNN-NNN");
+        MaskTextWatcher mtwCep = new MaskTextWatcher(cepEditText, smfCep);
+        cepEditText.addTextChangedListener(mtwCep);
+
+        //final da mascara de CEP
+
+
+
+
+
+
+
     }
+
 
     public void telaMeuPerfil(View view){
         Intent intent2 = new Intent(getApplicationContext(), MeuPerfilActivity.class);
