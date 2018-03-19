b92a4f920a1a61473f2e8bdf6f7132f7b774fde5
DanielM2305 <danielmarques2305@gmail.com>
2018-03-19 19:01:38 -0300

    Implementando Menu de botões e Nome dos Activities de cada tela

---
+++ app/src/main/java/casalimpa/projeto/com/casalimpa/MenuOpcao.java
@@ -0,0 +1,49 @@
+package casalimpa.projeto.com.casalimpa;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MenuOpcao extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_meu_perfil:
+                    mTextMessage.setText(R.string.meu_perfil);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_avaliacoes:
+                    mTextMessage.setText(R.string.avaliacoes);
+                    return true;
+                case R.id.navigation_servicos:
+                    mTextMessage.setText(R.string.servicos);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+
+}
